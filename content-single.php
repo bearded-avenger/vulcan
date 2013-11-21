d951afd05e2fa136a19d85ffa4cdc6e3621d685d
Bearded Avenger <email@nickhaskins.com>
2013-11-21 16:08:35 -0600

    do sharing and style

---
+++ content-single.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @package Soren
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+	<time class="soren-entry-date" datetime="<?php the_date('F jS, Y'); ?>" itemprop="datePublished" pubdate><?php echo the_time('F jS, Y'); ?></time>
+
+	<h2 class="soren-entry-title" itemprop="title"><a class="soren-fader" href="<?php the_permalink();?>"><?php the_title();?></a></h2>
+
+	<section class="soren-entry-content">
+		<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'soren' ) ); ?>
+	</section>
+
+	<?php echo soren_post_shares();?>
+</article><!-- #post-## -->
