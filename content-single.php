eeeb030820703903496af3fa04edfbae78bc6ce0
Bearded Avenger <email@nickhaskins.com>
2013-11-21 20:09:16 -0600

    do post author and style

--- content-single.php
+++ content-single.php
@@ -6,13 +6,22 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-	<time class="soren-entry-date" datetime="<?php the_date('F jS, Y'); ?>" itemprop="datePublished" pubdate><?php echo the_time('F jS, Y'); ?></time>
+	<header class="row">
+
+		<time class="soren-entry-date col-md-9" datetime="<?php the_date('F jS, Y'); ?>" itemprop="datePublished" pubdate><?php echo the_time('F jS, Y'); ?></time>
+
+		<section class="vulcan-post-shares col-md-3">
+			<?php echo soren_post_shares(true);?>
+		</section>
+	</header>
 
 	<h2 class="soren-entry-title" itemprop="title"><a class="soren-fader" href="<?php the_permalink();?>"><?php the_title();?></a></h2>
 
-	<section class="soren-entry-content">
+	<section class="soren-entry-content" itemprop="content">
 		<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'soren' ) ); ?>
 	</section>
 
-	<?php echo soren_post_shares(true);?>
+	<footer class="vulcan-post-footer row">
+		<?php echo soren_post_author();?>
+	</footer>
 </article><!-- #post-## -->
