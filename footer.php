2bc25ed8182e1b92f9c4e14a3c034d9850fe07ad
Bearded Avenger <email@nickhaskins.com>
2013-11-30 11:15:41 -0600

    add footer

---
+++ footer.php
@@ -0,0 +1,13 @@
+</main>
+<footer class="soren-content push">
+	<section class="soren-creds center">
+		<a href="http://wordpress.org/" rel="generator"><?php printf( __( 'Proudly powered by %s', 'soren' ), 'WordPress' ); ?></a>
+		<span class="sep"> | </span>
+		<?php printf( __( 'Theme: %s', 'soren' ),'<a href="http://nickhaskins.co/products/vulcan" rel="designer" target="_blank">Vulcan</a>' ); ?>
+	</section>
+</footer>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
